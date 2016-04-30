2efe741eea10cbb4a13b87880f5507c984ba7601
Sebastian Oleniacz <sebastian@sadtech.pl>
2016-05-01 00:20:11 +0200

    New catalog structure

---
+++ Release/Highcharts.framework/Headers/HIGChartView.h
@@ -0,0 +1,20 @@
+//
+//  HIGView.h
+//  Highcharts
+//
+//  License: www.highcharts.com/license
+//  Copyright © 2016 Highsoft AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HIGChartView : UIView
+
+@property (strong, nonatomic) NSDictionary *options;
+
+@property (strong, nonatomic) NSString *theme;
+@property (strong, nonatomic) NSArray *plugins;
+
+@property (assign, nonatomic) BOOL debug;
+
+@end
---
+++ Release/Highcharts.framework/Headers/UIColor+HIG.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+HIG.h
+//  Highcharts
+//
+//  License: www.highcharts.com/license
+//  Copyright © 2016 Highsoft AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HIG)
+
+- (NSString*)color;
+
+@end
