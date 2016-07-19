b8bbbc99ce11b78d59318d9980d8f274f9f1d5b4
Sebastian Oleniacz <sebastian@sadtech.pl>
2016-07-19 12:39:36 +0200

    API Parser for objectiveC

--- Tools/apidump.php
+++ Tools/apidump.php
@@ -1,27 +1,62 @@
 <?php
 	
-    function apiParse($PATH_IN, $PATH_OUT) {
+    function apiParse($PATH_IN, $PATH_OUT, $CLEAR) {
         $FILE_CONTENT_IN = file_get_contents($PATH_IN);
         
-        $FILE_CONTENT_OUT = $FILE_CONTENT_IN;
-		// Parse api here
-        
-        file_put_contents($PATH_OUT, $FILE_CONTENT_OUT);
+		$STR_SRCH[] = 'String'; 
+		$STR_REPL[] = 'NSString';
+
+		$STR_SRCH[] = 'Number'; 
+		$STR_REPL[] = 'NSNumber';
+
+		$STR_SRCH[] = 'Boolean'; 
+		$STR_REPL[] = 'BOOL';
+
+		$STR_SRCH[] = 'Boolean'; 
+		$STR_REPL[] = 'BOOL';
+
+		$STR_SRCH[] = 'Color';
+		$STR_REPL[] = 'UIColor';
+
+		$STR_SRCH[] = 'Array';
+		$STR_REPL[] = 'NSArray';
+
+		$STR_SRCH[] = 'Object';
+		$STR_REPL[] = 'NSObject';
+
+		$items = json_decode($FILE_CONTENT_IN);
+		
+		foreach ($items as &$item) {
+			
+			if ($CLEAR) {
+				$item->deprecated = FALSE;
+				$item->seeAlso = "";
+				$item->demo = "";
+				$item->since = "";
+			}
+
+			var_dump($item);
+		}
+		
+		// file_put_contents($PATH_OUT, $FILE_CONTENT_OUT);
     }
     
     if (!$argv[1]) {
-        echo 'usage: api2doc [input file] [output file]';
+        echo 'usage: apidump [input file] [output file] -c';
         echo "\n";
         exit;
     }
-    
+
     // directory to scan input
     $DIR_PATH_IN = $argv[1];
     
     // directory to output
     $DIR_PATH_OUT = $argv[2];
-    
-    apiParse($DIR_PATH_IN, $DIR_PATH_OUT);
+
+    // clear unused items
+    $CLEAR = $argv[3] ? TRUE : FALSE;
+
+    apiParse($DIR_PATH_IN, $DIR_PATH_OUT, $CLEAR);
     
     exit;
 ?>
