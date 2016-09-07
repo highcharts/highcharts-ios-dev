b8410cbdb090e7ef4b488741066fc68e99e54730
Sebastian Oleniacz <sebastian@sadtech.pl>
2016-09-07 15:54:56 +0200

    Example: HighFit, new data added calories out

--- Release/Example/HighFit/HighFit/DataTableViewController.m
+++ Release/Example/HighFit/HighFit/DataTableViewController.m
@@ -31,7 +31,7 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
-    self.chartType = @"column";
+    self.chartType = self.configuration[@"chartType"];
     
     self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
     
