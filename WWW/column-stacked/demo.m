7ead9e9ed20ec3cf2d28a83ea7aaf9a0d59fce06
Sebastian Oleniacz <sebastian@sadtech.pl>
2016-11-24 17:27:11 +0100

    Example: New demos for www

---
+++ WWW/column-stacked/demo.m
@@ -0,0 +1,113 @@
+//
+//  ViewController.m
+//  Highcharts
+//
+//  License: www.highcharts.com/license
+//  Copyright © 2016 Highsoft AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Highcharts/Highcharts.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    HIGChartView *chartView = [[HIGChartView alloc] initWithFrame:self.view.bounds];
+    
+    self.chartView.options = @{
+                               @"chart": @{
+                                       @"type": @"column"
+                                       },
+                               @"title": @{
+                                       @"text": @"Stacked column chart"
+                                       },
+                               @"xAxis": @{
+                                       @"categories": @[
+                                               @"Apples",
+                                               @"Oranges",
+                                               @"Pears",
+                                               @"Grapes",
+                                               @"Bananas"
+                                               ]
+                                       },
+                               @"yAxis": @{
+                                       @"min": @0,
+                                       @"title": @{
+                                               @"text": @"Total fruit consumption"
+                                               },
+                                       @"stackLabels": @{
+                                               @"enabled": @true,
+                                               @"style": @{
+                                                       @"fontWeight": @"bold"
+                                                       }
+                                               }
+                                       },
+                               @"legend": @{
+                                       @"align": @"right",
+                                       @"x": @-30,
+                                       @"verticalAlign": @"top",
+                                       @"y": @25,
+                                       @"floating": @true,
+                                       @"borderColor": @"#CCC",
+                                       @"borderWidth": @1,
+                                       @"shadow": @false
+                                       },
+                               @"tooltip": @{
+                                       @"headerFormat": @"<b>{point.x}</b><br/>",
+                                       @"pointFormat": @"{series.name}: {point.y}<br/>Total: {point.stackTotal}"
+                                       },
+                               @"plotOptions": @{
+                                       @"column": @{
+                                               @"stacking": @"normal",
+                                               @"dataLabels": @{
+                                                       @"enabled": @true,
+                                                       @"style": @{
+                                                               @"textShadow": @"0 0 3px black"
+                                                               }
+                                                       }
+                                               }
+                                       },
+                               @"series": @[
+                                       @{
+                                           @"name": @"John",
+                                           @"data": @[
+                                                   @5,
+                                                   @3,
+                                                   @4,
+                                                   @7,
+                                                   @2
+                                                   ]
+                                           },
+                                       @{
+                                           @"name": @"Jane",
+                                           @"data": @[
+                                                   @2,
+                                                   @2,
+                                                   @3,
+                                                   @2,
+                                                   @1
+                                                   ]
+                                           },
+                                       @{
+                                           @"name": @"Joe",
+                                           @"data": @[
+                                                   @3,
+                                                   @4,
+                                                   @4,
+                                                   @2,
+                                                   @5
+                                                   ]
+                                           }
+                                       ]
+                               };
+    
+    [self.view addSubview:self.chartView];
+}
+
+@end
---
+++ WWW/pie-legend/demo.m
@@ -0,0 +1,85 @@
+//
+//  ViewController.m
+//  Highcharts
+//
+//  License: www.highcharts.com/license
+//  Copyright © 2016 Highsoft AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Highcharts/Highcharts.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    HIGChartView *chartView = [[HIGChartView alloc] initWithFrame:self.view.bounds];
+    
+    self.chartView.options = @{
+                               @"chart": @{
+                                       @"plotBackgroundColor": [NSNull null],
+                                       @"plotBorderWidth": [NSNull null],
+                                       @"plotShadow": @false,
+                                       @"type": @"pie"
+                                       },
+                               @"title": @{
+                                       @"text": @"Browser market shares January, 2015 to May, 2015"
+                                       },
+                               @"tooltip": @{
+                                       @"pointFormat": @"{series.name}: <b>{point.percentage:.1f}%</b>"
+                                       },
+                               @"plotOptions": @{
+                                       @"pie": @{
+                                               @"allowPointSelect": @true,
+                                               @"cursor": @"pointer",
+                                               @"dataLabels": @{
+                                                       @"enabled": @false
+                                                       },
+                                               @"showInLegend": @true
+                                               }
+                                       },
+                               @"series": @[
+                                       @{
+                                           @"name": @"Brands",
+                                           @"colorByPoint": @true,
+                                           @"data": @[
+                                                   @{
+                                                       @"name": @"Microsoft Internet Explorer",
+                                                       @"y": @56.33
+                                                       },
+                                                   @{
+                                                       @"name": @"Chrome",
+                                                       @"y": @24.03,
+                                                       @"sliced": @true,
+                                                       @"selected": @true
+                                                       },
+                                                   @{
+                                                       @"name": @"Firefox",
+                                                       @"y": @10.38
+                                                       },
+                                                   @{
+                                                       @"name": @"Safari",
+                                                       @"y": @4.77
+                                                       },
+                                                   @{
+                                                       @"name": @"Opera",
+                                                       @"y": @0.91
+                                                       },
+                                                   @{
+                                                       @"name": @"Proprietary or Undetectable",
+                                                       @"y": @0.2
+                                                       }
+                                                   ]
+                                           }
+                                       ]
+                               };
+    
+    [self.view addSubview:self.chartView];
+}
+
+@end
