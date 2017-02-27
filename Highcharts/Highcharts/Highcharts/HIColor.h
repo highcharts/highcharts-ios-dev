a3f8ff75264af260b52ef54981fa28a2bba97e43
Krzysztof <krzysztof.fryzlewicz@spinney.io>
2017-02-27 08:56:52 +0100

    Change from HIHexColor to HIColor

---
+++ Highcharts/Highcharts/Highcharts/HIColor.h
@@ -0,0 +1,29 @@
+//
+//  HIColor.h
+//  Highcharts
+//
+//  Created by krzysiek on 27.02.2017.
+//  Copyright © 2017 Highsoft AS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HIColor : NSObject
+
+@property(nonatomic, readwrite) NSString *string;
+/** 
+ * Standard hex color value, for example FFFFFF for black and 000000 for white
+ */
+-(instancetype)initWithHexValue:(NSString *)string;
+/**
+ * Values of red, green and blue color in RGB standard: values 0-255
+ */
+-(instancetype)initWithRGB:(int)red green:(int)green blue:(int)blue;
+/**
+ * Values of red, green and blue color in RGBa standard. Values for colors: 0-255, alpha: 0-1
+ */
+-(instancetype)initWithRGBA:(int)red green:(int)green blue:(int)blue alpha:(double)alpha;
+
+-(NSString *)getString;
+
+@end
