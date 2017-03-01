8d856e1f7a4a67491d14007b524ae40fc009aa24
Krzysztof <krzysztof.fryzlewicz@spinney.io>
2017-03-01 14:52:09 +0100

    added another demos

--- www/gauge-speedometer/demo-dark-unica.m
+++ www/gauge-speedometer/demo-dark-unica.m
@@ -22,124 +22,81 @@
 
     chartView.theme = @"dark-unica";
     
-    chartView.options = @{
-    @"chart": @{
-        @"type": @"gauge",
-        @"plotBackgroundColor": [NSNull null],
-        @"plotBackgroundImage": [NSNull null],
-        @"plotBorderWidth": @0,
-        @"plotShadow": @false
-    },
-    @"title": @{
-        @"text": @"Speedometer"
-    },
-    @"pane": @{
-        @"startAngle": @-150,
-        @"endAngle": @150,
-        @"background": @[
-            @{
-                @"backgroundColor": @{
-                    @"linearGradient": @{
-                        @"x1": @0,
-                        @"y1": @0,
-                        @"x2": @0,
-                        @"y2": @1
-                    },
-                    @"stops": @[
-                        @[
-                            @0,
-                            @"#FFF"
-                        ],
-                        @[
-                            @1,
-                            @"#333"
-                        ]
-                    ]
-                },
-                @"borderWidth": @0,
-                @"outerRadius": @"109%"
-            },
-            @{
-                @"backgroundColor": @{
-                    @"linearGradient": @{
-                        @"x1": @0,
-                        @"y1": @0,
-                        @"x2": @0,
-                        @"y2": @1
-                    },
-                    @"stops": @[
-                        @[
-                            @0,
-                            @"#333"
-                        ],
-                        @[
-                            @1,
-                            @"#FFF"
-                        ]
-                    ]
-                },
-                @"borderWidth": @1,
-                @"outerRadius": @"107%"
-            },
-            @{},
-            @{
-                @"backgroundColor": @"#DDD",
-                @"borderWidth": @0,
-                @"outerRadius": @"105%",
-                @"innerRadius": @"103%"
-            }
-        ]
-    },
-    @"yAxis": @{
-        @"min": @0,
-        @"max": @200,
-        @"minorTickInterval": @"auto",
-        @"minorTickWidth": @1,
-        @"minorTickLength": @10,
-        @"minorTickPosition": @"inside",
-        @"minorTickColor": @"#666",
-        @"tickPixelInterval": @30,
-        @"tickWidth": @2,
-        @"tickPosition": @"inside",
-        @"tickLength": @10,
-        @"tickColor": @"#666",
-        @"labels": @{
-            @"step": @2,
-            @"rotation": @"auto"
-        },
-        @"title": @{
-            @"text": @"km/h"
-        },
-        @"plotBands": @[
-            @{
-                @"from": @0,
-                @"to": @120,
-                @"color": @"#55BF3B"
-            },
-            @{
-                @"from": @120,
-                @"to": @160,
-                @"color": @"#DDDF0D"
-            },
-            @{
-                @"from": @160,
-                @"to": @200,
-                @"color": @"#DF5353"
-            }
-        ]
-    },
-    @"series": @[
-        @{
-            @"name": @"Speed",
-            @"data": @[
-                @80
-            ],
-            @"tooltip": @{
-                @"valueSuffix": @" km/h"
-            }
-        }
-    ]
-};
+    HIOptions *options = [[HIOptions alloc]init];
+    
+    HIChart *chart = [[HIChart alloc]init];
+    chart.type = @"gauge";
+    chart.plotBorderWidth = @0;
+    chart.plotShadow = @false;
+    
+    HITitle *title = [[HITitle alloc]init];
+    title.text = @"Speedometer";
+    
+    HIPane *pane = [[HIPane alloc]init];
+    pane.startAngle = @-150;
+    pane.endAngle = @150;
+    HIPaneBackground *background1 = [[HIPaneBackground alloc]init];
+    background1.backgroundColor = [[HIColor alloc]initWithHexValue:@"333"];
+    background1.borderWidth = @0;
+    background1.outerRadius = @"109%";
+    HIPaneBackground *background2 = [[HIPaneBackground alloc]init];
+    background2.backgroundColor = [[HIColor alloc]initWithHexValue:@"FFF"];
+    background2.borderWidth = @1;
+    background2.outerRadius = @"107%";
+    HIPaneBackground *background3 = [[HIPaneBackground alloc]init];
+    HIPaneBackground *background4 = [[HIPaneBackground alloc]init];
+    background4.backgroundColor = [[HIColor alloc]initWithHexValue:@"DDD"];
+    background4.borderWidth = @0;
+    background4.outerRadius = @"105%";
+    background4.innerRadius = @"103%";
+    pane.background = [NSMutableArray arrayWithObjects:background1, background2, background3, background4, nil];
+    
+    HIYAxis *yaxis = [[HIYAxis alloc]init];
+    yaxis.min = @0;
+    yaxis.max = @200;
+    yaxis.minorTickInterval = @"auto";
+    yaxis.minorTickWidth = @1;
+    yaxis.minorTickLength = @10;
+    yaxis.minorTickPosition = @"inside";
+    yaxis.minorTickColor = [[HIColor alloc]initWithHexValue:@"666"];
+    yaxis.tickPixelInterval = @30;
+    yaxis.tickWidth = @2;
+    yaxis.tickPosition = @"inside";
+    yaxis.tickLength = @10;
+    yaxis.tickColor = [[HIColor alloc]initWithHexValue:@"666"];
+    yaxis.labels = [[HIYAxisLabels alloc]init];
+    yaxis.labels.step = @2;
+    yaxis.labels.rotation = @"auto";
+    yaxis.title = [[HIYAxisTitle alloc]init];
+    yaxis.title.text = @"km/h";
+    HIYAxisPlotBands *plotband1 = [[HIYAxisPlotBands alloc]init];
+    plotband1.from = @0;
+    plotband1.to = @120;
+    plotband1.color = [[HIColor alloc]initWithHexValue:@"55BF3B"];
+    HIYAxisPlotBands *plotband2 = [[HIYAxisPlotBands alloc]init];
+    plotband2.from = @120;
+    plotband2.to = @160;
+    plotband2.color = [[HIColor alloc]initWithHexValue:@"DDDF0D"];
+    HIYAxisPlotBands *plotband3 = [[HIYAxisPlotBands alloc]init];
+    plotband3.from = @160;
+    plotband3.to = @200;
+    plotband3.color = [[HIColor alloc]initWithHexValue:@"DF5353"];
+    yaxis.plotBands = [NSMutableArray arrayWithObjects:plotband1, plotband2, plotband3, nil];
+    
+    
+    HIGauge *gauge = [[HIGauge alloc]init];
+    gauge.name = @"Speed";
+    gauge.tooltip = [[HIGaugeTooltip alloc]init];
+    gauge.tooltip.valueSuffix = @" km/h";
+    gauge.data = [NSMutableArray arrayWithObject:@80];
+    
+    options.chart = chart;
+    options.title = title;
+    options.pane = pane;
+    options.yAxis = [NSMutableArray arrayWithObjects:yaxis, nil];
+    options.series = [NSMutableArray arrayWithObjects: gauge, nil];
+    
+    chartView.options = options;
     
     [self.view addSubview:chartView];
 }
--- www/gauge-vu-meter/demo.m
+++ www/gauge-vu-meter/demo.m
@@ -20,140 +20,67 @@
     
     HIGChartView *chartView = [[HIGChartView alloc] initWithFrame:self.view.bounds];
     
-    chartView.options = @{
-                            @"chart": @{
-                                @"type": @"gauge",
-                                @"plotBorderWidth": @1,
-                                @"plotBackgroundColor": @{
-                                    @"linearGradient": @{
-                                        @"x1": @0,
-                                        @"y1": @0,
-                                        @"x2": @0,
-                                        @"y2": @1
-                                    },
-                                    @"stops": @[
-                                        @[
-                                            @0,
-                                            @"#FFF4C6"
-                                        ],
-                                        @[
-                                            @0.3,
-                                            @"#FFFFFF"
-                                        ],
-                                        @[
-                                            @1,
-                                            @"#FFF4C6"
-                                        ]
-                                    ]
-                                },
-                                @"plotBackgroundImage": [NSNull null],
-                                @"height": @200
-                            },
-                            @"title": @{
-                                @"text": @"VU meter"
-                            },
-                            @"pane": @[
-                                @{
-                                    @"startAngle": @-45,
-                                    @"endAngle": @45,
-                                    @"background": [NSNull null],
-                                    @"center": @[
-                                        @"25%",
-                                        @"145%"
-                                    ],
-                                    @"size": @300
-                                },
-                                @{
-                                    @"startAngle": @-45,
-                                    @"endAngle": @45,
-                                    @"background": [NSNull null],
-                                    @"center": @[
-                                        @"75%",
-                                        @"145%"
-                                    ],
-                                    @"size": @300
-                                }
-                            ],
-                            @"tooltip": @{
-                                @"enabled": @false
-                            },
-                            @"yAxis": @[
-                                @{
-                                    @"min": @-20,
-                                    @"max": @6,
-                                    @"minorTickPosition": @"outside",
-                                    @"tickPosition": @"outside",
-                                    @"labels": @{
-                                        @"rotation": @"auto",
-                                        @"distance": @20
-                                    },
-                                    @"plotBands": @[
-                                        @{
-                                            @"from": @0,
-                                            @"to": @6,
-                                            @"color": @"#C02316",
-                                            @"innerRadius": @"100%",
-                                            @"outerRadius": @"105%"
-                                        }
-                                    ],
-                                    @"pane": @0,
-                                    @"title": @{
-                                        @"text": @"VU<br/><span style=\"font-size:8px\">Channel A</span>",
-                                        @"y": @-40
-                                    }
-                                },
-                                @{
-                                    @"min": @-20,
-                                    @"max": @6,
-                                    @"minorTickPosition": @"outside",
-                                    @"tickPosition": @"outside",
-                                    @"labels": @{
-                                        @"rotation": @"auto",
-                                        @"distance": @20
-                                    },
-                                    @"plotBands": @[
-                                        @{
-                                            @"from": @0,
-                                            @"to": @6,
-                                            @"color": @"#C02316",
-                                            @"innerRadius": @"100%",
-                                            @"outerRadius": @"105%"
-                                        }
-                                    ],
-                                    @"pane": @1,
-                                    @"title": @{
-                                        @"text": @"VU<br/><span style=\"font-size:8px\">Channel B</span>",
-                                        @"y": @-40
-                                    }
-                                }
-                            ],
-                            @"plotOptions": @{
-                                @"gauge": @{
-                                    @"dataLabels": @{
-                                        @"enabled": @false
-                                    },
-                                    @"dial": @{
-                                        @"radius": @"100%"
-                                    }
-                                }
-                            },
-                            @"series": @[
-                                @{
-                                    @"name": @"Channel A",
-                                    @"data": @[
-                                        @-8
-                                    ],
-                                    @"yAxis": @0
-                                },
-                                @{
-                                    @"name": @"Channel B",
-                                    @"data": @[
-                                        @-2
-                                    ],
-                                    @"yAxis": @1
-                                }
-                            ]
-                        };
+    HIOptions *options = [[HIOptions alloc]init];
+    
+    HIChart *chart = [[HIChart alloc]init];
+    chart.type = @"gauge";
+    chart.plotBorderWidth = @1;
+    chart.plotBackgroundColor = [[HIColor alloc]initWithHexValue:@"FFF4C6"];
+    chart.height = @200;
+    
+    HITitle *title = [[HITitle alloc]init];
+    title.text = @"VU meter";
+    
+    HIPane *pane = [[HIPane alloc]init];
+    pane.startAngle = @-45;
+    pane.endAngle = @45;
+    pane.background = [[NSMutableArray alloc]init];
+    pane.size = @300;
+    pane.center = [NSMutableArray arrayWithObjects:@"50%", @"130%", nil];
+    
+    HITooltip *tooltip = [[HITooltip alloc]init];
+    tooltip.enabled = @false;
+    
+    HIYAxis *yaxis = [[HIYAxis alloc]init];
+    yaxis.min = @-20;
+    yaxis.max = @6;
+    yaxis.minorTickPosition = @"outside";
+    yaxis.tickPosition = @"outside";
+    yaxis.labels = [[HIYAxisLabels alloc]init];
+    yaxis.labels.distance = @20;
+    yaxis.labels.rotation = @"auto";
+    yaxis.title = [[HIYAxisTitle alloc]init];
+    yaxis.title.text = @"VU<br/><span style=\"font-size:8px\">Channel</span>";
+    yaxis.title.y = @-40;
+    HIYAxisPlotBands *plotband1 = [[HIYAxisPlotBands alloc]init];
+    plotband1.from = @0;
+    plotband1.to = @6;
+    plotband1.color = [[HIColor alloc]initWithHexValue:@"C02316"];
+    plotband1.innerRadius = @"100%";
+    plotband1.outerRadius = @"105%";
+    yaxis.plotBands = [NSMutableArray arrayWithObjects:plotband1, nil];
+    
+    HIPlotOptions *plotoptions = [[HIPlotOptions alloc]init];
+    plotoptions.gauge = [[HIPlotOptionsGauge alloc]init];
+    plotoptions.gauge.dataLabels = [[HIPlotOptionsGaugeDataLabels alloc]init];
+    plotoptions.gauge.dataLabels.enabled = @false;
+    plotoptions.gauge.dial = [[HIPlotOptionsGaugeDial alloc]init];
+    plotoptions.gauge.dial.radius = @"100%";
+    
+    
+    HIGauge *gauge = [[HIGauge alloc]init];
+    gauge.name = @"Channel";
+    gauge.data = [NSMutableArray arrayWithObject:@-8];
+    
+    options.chart = chart;
+    options.title = title;
+    options.pane = pane;
+    options.tooltip = tooltip;
+    options.plotOptions = plotoptions;
+    options.yAxis = [NSMutableArray arrayWithObjects:yaxis, nil];
+    options.series = [NSMutableArray arrayWithObjects: gauge, nil];
+    
+    chartView.options = options;
     
     [self.view addSubview:chartView];
 }
