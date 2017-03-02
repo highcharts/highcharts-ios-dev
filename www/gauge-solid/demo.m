153b670d66cf7875aa0daac28338b15a1fad850d
Krzysztof <krzysztof.fryzlewicz@spinney.io>
2017-03-02 14:43:04 +0100

    Added gauge-solid

--- www/gauge-solid/demo.m
+++ www/gauge-solid/demo.m
@@ -21,85 +21,81 @@
     
     HIGChartView *chartView = [[HIGChartView alloc] initWithFrame:self.view.bounds];
     
-    chartView.plugins = @[ @"solid-gauge" ];
-
-    chartView.options = @{
-                            @"chart": @{
-                                @"type": @"solidgauge"
-                            },
-                            @"title": [NSNull null],
-                            @"pane": @{
-                                @"center": @[
-                                    @"50%",
-                                    @"85%"
-                                ],
-                                @"size": @"140%",
-                                @"startAngle": @-90,
-                                @"endAngle": @90,
-                                @"background": @{
-                                    @"backgroundColor": @"#EEE",
-                                    @"innerRadius": @"60%",
-                                    @"outerRadius": @"100%",
-                                    @"shape": @"arc"
-                                }
-                            },
-                            @"tooltip": @{
-                                @"enabled": @false
-                            },
-                            @"plotOptions": @{
-                                @"solidgauge": @{
-                                    @"dataLabels": @{
-                                        @"y": @5,
-                                        @"borderWidth": @0,
-                                        @"useHTML": @true
-                                    }
-                                }
-                            },
-                            @"yAxis": @{
-                                @"stops": @[
-                                    @[
-                                        @0.1,
-                                        @"#55BF3B"
-                                    ],
-                                    @[
-                                        @0.5,
-                                        @"#DDDF0D"
-                                    ],
-                                    @[
-                                        @0.9,
-                                        @"#DF5353"
-                                    ]
-                                ],
-                                @"lineWidth": @0,
-                                @"minorTickInterval": [NSNull null],
-                                @"tickAmount": @2,
-                                @"title": @{
-                                    @"text": @"Speed"
-                                },
-                                @"labels": @{
-                                    @"y": @16
-                                },
-                                @"min": @0,
-                                @"max": @200
-                            },
-                            @"credits": @{
-                                @"enabled": @false
-                            },
-                            @"series": @[
-                                @{
-                                    @"name": @"Speed",
-                                    @"data": @[
-                                        @80
-                                    ],
-                                    @"dataLabels": @{
-                                        @"format": @"<div style=\"text-align:center\"><span style=\"font-size:25px;color:black\">{y}</span><br/><span style=\"font-size:12px;color:silver\">km/h</span></div>"
-                                    },
-                                    @"tooltip": @{
-                                        @"valueSuffix": @" km/h"
-                                    }
-                                }
-                            ]
-                        };
+    HIOptions *options = [[HIOptions alloc]init];
+    
+    HIChart *chart = [[HIChart alloc]init];
+    chart.type = @"solidgauge";
+    options.chart = chart;
+    
+    HITitle *title = [[HITitle alloc]init];
+    title.text = @"";
+    options.title = title;
+    
+    HIPane *pane = [[HIPane alloc]init];
+    pane.center = @[@"50%", @"85%"];
+    pane.size = @"140%";
+    pane.startAngle = @-90;
+    pane.endAngle = @90;
+    HIPaneBackground *background = [[HIPaneBackground alloc]init];
+    background.backgroundColor = [[HIColor alloc]initWithHexValue:@"EEE"];
+    background.innerRadius = @"60%";
+    background.outerRadius = @"100%";
+    background.shape = @"arc";
+    pane.background = @[background];
+    options.pane = pane;
+    
+    HITooltip *tooltip = [[HITooltip alloc]init];
+    tooltip.enabled = @false;
+    options.tooltip = tooltip;
+    
+    HIPlotOptions *plotoptions = [[HIPlotOptions alloc]init];
+    plotoptions.solidgauge = [[HIPlotOptionsSolidgauge alloc]init];
+    plotoptions.solidgauge.dataLabels = [[HIPlotOptionsSolidgaugeDataLabels alloc]init];
+    plotoptions.solidgauge.dataLabels.y = @5;
+    plotoptions.solidgauge.dataLabels.borderWidth = @0;
+    plotoptions.solidgauge.dataLabels.useHTML = @true;
+    options.plotOptions = plotoptions;
+    
+    HIYAxis *yaxis = [[HIYAxis alloc]init];
+    yaxis.stops = @[
+                    @[
+                        @0.1,
+                        @"#55BF3B"
+                        ],
+                    @[
+                        @0.5,
+                        @"#DDDF0D"
+                        ],
+                    @[
+                        @0.9,
+                        @"#DF5353"
+                        ]
+                    ];
+    yaxis.lineWidth = @0;
+    yaxis.minorTickInterval = [NSNull null];
+    yaxis.tickAmount = @2;
+    yaxis.title = [[HIYAxisTitle alloc]init];
+    yaxis.title.text = @"Speed";
+    yaxis.labels = [[HIYAxisLabels alloc]init];
+    yaxis.labels.y = @16;
+    yaxis.min = @0;
+    yaxis.max = @200;
+    options.yAxis = @[yaxis];
+    
+    HICredits *credits = [[HICredits alloc]init];
+    credits.enabled = @false;
+    options.credits = credits;
+    
+    HISolidgauge *gauge = [[HISolidgauge alloc]init];
+    gauge.name = @"Speed";
+    gauge.data = @[@80];
+    gauge.dataLabels = [[HISolidgaugeDataLabels alloc]init];
+    gauge.dataLabels.format = @"<div style=\"text-align:center\"><span style=\"font-size:25px;color:black\">{y}</span><br/><span style=\"font-size:12px;color:silver\">km/h</span></div>";
+    gauge.tooltip = [[HISolidgaugeTooltip alloc]init];
+    gauge.tooltip.valueSuffix = @" km/h";
+    options.series = @[gauge];
+    
+    chartView.options = options;
     
     [self.view addSubview:chartView];
 }
