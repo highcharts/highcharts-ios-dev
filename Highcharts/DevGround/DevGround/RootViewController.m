ce529135c5407508c51d53a56e597389cb7951ca
Maciej Trystuła <maciej@Mac-mini.local>
2017-03-02 14:17:31 +0100

    Init

--- Highcharts/DevGround/DevGround/RootViewController.m
+++ Highcharts/DevGround/DevGround/RootViewController.m
@@ -7,24 +7,62 @@
 //
 
 #import "RootViewController.h"
-#import "ViewController.h"
+#import <Highcharts/Highcharts.h>
+#import "ChartType.h"
+
+
+@interface RootViewController () {
+    NSArray *_pickerData;
+}
+
+@end
 
 @implementation RootViewController
 
+
+
 -(void)viewDidLoad {
     [super viewDidLoad];
+    _pickerData = @[@"Pie", @"Scatter", @"Polar", @"Area", @"Line", @"Bar", @"Column", @"Bubble", @"Funnel", @"Box plot", @"Waterfall", @"Error bar", @"Spline"];
+    self.chartTypePicker.dataSource = self;
+    self.chartTypePicker.delegate = self;
     
-    for (UIButton *button in self.typeButtons) {
-        [button addTarget:self action:@selector(typeButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
-    }
-    
+    [self.proceedButton addTarget:self action:@selector(proceedButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
 }
 
--(void)typeButtonTapped:(UIButton *)sender {
-    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    ViewController *vc = [storyboard instantiateViewControllerWithIdentifier:@"ChartVC"];
-    vc.chartType = sender.titleLabel.text;
-    [self.navigationController pushViewController:vc animated:false];
+-(void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.chartView = [[HIGChartView alloc] initWithFrame:CGRectMake(self.myChartView.bounds.origin.x, self.myChartView.bounds.origin.y , self.myChartView.bounds.size.width, 300.0f)];
+    
+   // self.chartView.options = [ChartType getChartByType:self.chartType];
 }
+
+-(void)proceedButtonTapped:(UIButton *)sender {
+    for (UIView* subView in self.myChartView.subviews) {
+        [subView removeFromSuperview];
+    }
+    NSString *yourSelectedTitle = [_pickerData objectAtIndex:[self.chartTypePicker selectedRowInComponent:0]];
+    self.chartView.options = [ChartType getChartByType: yourSelectedTitle];
+    [self.myChartView addSubview:self.chartView];
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+// The number of rows of data
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return _pickerData.count;
+}
+
+// The data to return for the row and component (column) that's being passed in
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return _pickerData[row];
+}
+
+    
 
 @end
