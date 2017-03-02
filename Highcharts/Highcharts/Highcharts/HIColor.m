55f37438a9a144b36ffbc37ca6544a6f216e8f00
Maciej Trystuła <maciej@Mac-mini.local>
2017-03-02 14:28:37 +0100

    Apply HIColor Changes

--- Highcharts/Highcharts/Highcharts/HIColor.m
+++ Highcharts/Highcharts/Highcharts/HIColor.m
@@ -11,77 +11,61 @@
 
 @implementation HIColor
 
--(instancetype)initWithHexValue:(NSString *)hex {
+-(instancetype)initWithString:(NSString *)string {
     if (self = [super init]) {
-        self.string = [NSString stringWithFormat:@"#%@", hex];
+        self.string = string;
+        self.dictionary = nil;
         return self;
     }
     else {
         return nil;
     }
+}
+
+-(instancetype)initWithDictionary:(NSDictionary *)dict {
+    if (self = [super init]) {
+        self.string = nil;
+        self.dictionary = dict;
+        return self;
+    }
+    else {
+        return nil;
+    }
+}
+
+-(instancetype)initWithHexValue:(NSString *)hex {
+    return [self initWithString:[NSString stringWithFormat:@"#%@", hex]];
 }
 
 -(instancetype)initWithRGB:(int)red green:(int)green blue:(int)blue {
-    if (self = [super init]) {
-        self.string = [NSString stringWithFormat:@"rgb(%d, %d, %d)", red, green, blue];
-        return self;
-    }
-    else {
-        return nil;
-    }
+    return [self initWithString:self.string = [NSString stringWithFormat:@"rgb(%d, %d, %d)", red, green, blue]];
 }
 
 -(instancetype)initWithRGBA:(int)red green:(int)green blue:(int)blue alpha:(double)alpha {
-    if (self = [super init]) {
-        if (alpha >= 0.0 && alpha <= 1.0) {
-            self.string = [NSString stringWithFormat:@"rgba(%d, %d, %d,%f)", red, green, blue, alpha];
-        }
-        else {
-            self.string = [NSString stringWithFormat:@"rgba(%d, %d, %d,%f)", red, green, blue, 1.0];
-        }
-        return self;
-    }
-    else {
-        return nil;
-    }
+    return [self initWithString:[NSString stringWithFormat:@"rgba(%d, %d, %d,%f)", red, green, blue, alpha]];
 }
 
 -(instancetype)initWithName:(NSString *)name {
-    if (self = [super init]) {
-        self.string = name;
-        return self;
-    }
-    else {
-        return nil;
-    }
+    return [self initWithString:name];
 }
 
 -(instancetype)initWithLinearGradient:(NSDictionary *)gradient stops:(NSArray *)stops {
-    if (self = [super init]) {
-        self.dictionary = [NSDictionary dictionaryWithObjectsAndKeys:gradient, @"linearGradient", stops, @"stops", nil];
-        return self;
-    }
-    else {
-        return nil;
-    }
+    return [self initWithDictionary:[NSDictionary dictionaryWithObjectsAndKeys:gradient, @"linearGradient", stops, @"stops", nil]];
 }
 
 -(instancetype)initWithRadialGradient:(NSDictionary *)gradient stops:(NSArray *)stops {
-    if (self = [super init]) {
-        self.dictionary = [NSDictionary dictionaryWithObjectsAndKeys:gradient, @"radialGradient", stops, @"stops", nil];
-        return self;
-    }
-    else {
-        return nil;
-    }
+    return [self initWithDictionary:[NSDictionary dictionaryWithObjectsAndKeys:gradient, @"radialGradient", stops, @"stops", nil]];
 }
 
 -(id)getData {
-    if (self.dictionary.count > 0) {
+    if (self.dictionary) {
         return self.dictionary;
     }
-    else {
+    else if (self.string) {
         return self.string;
+    }
+    else {
+        return nil;
     }
 }
 
