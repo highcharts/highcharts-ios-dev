8b27e458d939648900212bf959c9195ff4dd9553
Krzysztof <krzysztof.fryzlewicz1@gmail.com>
2017-07-06 15:22:35 +0200

    created script for archiving

---
+++ Highcharts/Highcharts/archivingscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p "Build"
+
+xcodebuild -project Highcharts.xcodeproj -scheme Highcharts -sdk iphoneos SYMROOT=Build -arch armv7 -arch armv7s -arch arm64 only_active_arch=no
+
+xcodebuild archive -project Highcharts.xcodeproj -scheme Highcharts -sdk iphoneos SYMROOT=Build -archivePath Archive -arch armv7 -arch armv7s -arch arm64
+
+xcodebuild build -project Highcharts.xcodeproj -scheme Highcharts -sdk iphonesimulator SYMROOT=Build only_active_arch=no -arch x86_64 -arch i386
+
+cp -RL Archive.xcarchive/Products/Library/Frameworks/ Build/Release-universal/
+
+lipo -create Archive.xcarchive/Products/Library/Frameworks/Highcharts.framework/Highcharts Build/Release-iphonesimulator/Highcharts.framework/Highcharts -output Build/Release-universal/Highcharts.framework/Highcharts
+
+cp -RL Build/Release-universal/ Ready/
+
+rm -dr Build
+
+rm -dr Archive.xcarchive
