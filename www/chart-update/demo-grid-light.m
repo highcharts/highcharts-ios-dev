12bc43c59aa22903ea157aab1e6401d896fdd704
Ihnat Moisieiev <ignat@pc42.home>
2017-09-29 15:55:20 +0200

    [#151286116] - Update options after render

---
+++ www/chart-update/demo-grid-light.m
@@ -0,0 +1,118 @@
+#import <UIKit/UIKit.h>
+#import <Highcharts/Highcharts.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+HIGChartView *chartView;
+NSString *currentButtonClicked;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    chartView = [[HIGChartView alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x, self.view.bounds.origin.y, self.view.bounds.size.width, self.view.bounds.size.height - 50)];
+    
+    chartView.theme = @"grid-light";
+    
+    HIOptions *options = [[HIOptions alloc] init];
+    
+    HIChart *chart = [[HIChart alloc] init];
+    chart.type = @"column";
+    
+    HITitle *title = [[HITitle alloc] init];
+    title.text = @"Chart.update";
+    
+    HISubtitle *subtitle = [[HISubtitle alloc] init];
+    subtitle.text = @"Plain";
+    currentButtonClicked = @"Plain";
+    
+    HIXAxis *xaxis = [[HIXAxis alloc]init];
+    xaxis.categories = [NSMutableArray arrayWithObjects:@"Jan", @"Feb", @"Mar", @"Apr", @"May", @"Jun", @"Jul", @"Aug", @"Sep", @"Oct", @"Nov", @"Dec", nil];
+    
+    HIColumn *column = [[HIColumn alloc] init];
+    column.colorByPoint = @true;
+    column.data = [NSMutableArray arrayWithObjects:@29.9, @71.5, @106.4, @129.2, @144.0, @176.0, @135.6, @148.5, @216.4, @194.1, @95.6, @54.4, nil];
+    column.showInLegend = @false;
+    
+    options.chart = chart;
+    options.title = title;
+    options.subtitle = subtitle;
+    options.xAxis = [NSMutableArray arrayWithObjects:xaxis, nil];
+    options.series = [NSMutableArray arrayWithObjects:column, nil];
+    
+    chartView.options = options;
+    
+    [self.view addSubview:chartView];
+    
+    [self addButton:@"Plain" atXPosition:50];
+    [self addButton:@"Inverted" atXPosition:160];
+    [self addButton:@"Polar" atXPosition:270];
+}
+
+- (void)addButton:(NSString*)name atXPosition:(CGFloat)x{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:name forState:UIControlStateNormal];
+    button.frame = CGRectMake(x, self.view.bounds.size.height - 40, 60, 30);
+    [self.view addSubview:button];
+}
+
+- (void)buttonPressed:(UIButton*)sender{
+    NSString* buttonTitle = sender.currentTitle;
+    if ([buttonTitle isEqualToString:currentButtonClicked]) {
+        return;
+    }
+    
+    HIOptions *newOptions = [[HIOptions alloc] init];
+    
+    HIChart *chart = [[HIChart alloc] init];
+    chart.type = @"column";
+    
+    HISubtitle *subtitle = [[HISubtitle alloc] init];
+    
+    if([buttonTitle isEqualToString:@"Plain"]) {
+        chart.inverted = @false;
+        chart.polar = @false;
+        subtitle.text = buttonTitle;
+        currentButtonClicked = buttonTitle;
+    }
+    else if ([buttonTitle isEqualToString:@"Inverted"]) {
+        chart.inverted = @true;
+        chart.polar = @false;
+        subtitle.text = buttonTitle;
+        currentButtonClicked = buttonTitle;
+    }
+    else if ([buttonTitle isEqualToString:@"Polar"]) {
+        chart.inverted = @false;
+        chart.polar = @true;
+        subtitle.text = buttonTitle;
+        currentButtonClicked = buttonTitle;
+    }
+    
+    HITitle *title = [[HITitle alloc] init];
+    title.text = @"Chart.update";
+    
+    HIXAxis *xaxis = [[HIXAxis alloc] init];
+    xaxis.categories = [NSMutableArray arrayWithObjects:@"Jan", @"Feb", @"Mar", @"Apr", @"May", @"Jun", @"Jul", @"Aug", @"Sep", @"Oct", @"Nov", @"Dec", nil];
+    
+    HIColumn *column = [[HIColumn alloc] init];
+    column.colorByPoint = @true;
+    column.data = [NSMutableArray arrayWithObjects:@29.9, @71.5, @106.4, @129.2, @144.0, @176.0, @135.6, @148.5, @216.4, @194.1, @95.6, @54.4, nil];
+    column.showInLegend = @false;
+    
+    newOptions.chart = chart;
+    newOptions.title = title;
+    newOptions.subtitle = subtitle;
+    newOptions.xAxis = [NSMutableArray arrayWithObjects:xaxis, nil];
+    newOptions.series = [NSMutableArray arrayWithObjects:column, nil];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        chartView.options = newOptions;
+    });
+}
+
+@end
+
