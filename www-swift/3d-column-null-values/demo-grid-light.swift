f13d70cf4e106bc74a1d665adee51a6f9a3c97fb
Ihnat Moisieiev <ihnat.moisieiev@spinney.io>
2020-07-09 18:41:52 +0200

    [#172273753] - 3d-column-null-values

---
+++ www-swift/3d-column-null-values/demo-grid-light.swift
@@ -0,0 +1,54 @@
+import Highcharts
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let chartView = HIChartView(frame: view.bounds)
+    chartView.theme = "grid-light"
+
+    let options = HIOptions()
+
+    let chart = HIChart()
+    chart.type = "column"
+    chart.options3d = HIOptions3d()
+    chart.options3d.enabled = true
+    chart.options3d.alpha = 10
+    chart.options3d.beta = 25
+    chart.options3d.depth = 70
+    options.chart = chart
+
+    let title = HITitle()
+    title.text = "3D chart with null values"
+    options.title = title
+
+    let subtitle = HISubtitle()
+    subtitle.text = "Notice the difference between a 0 value and a null point"
+    options.subtitle = subtitle
+
+    let plotOptions = HIPlotOptions()
+    plotOptions.column = HIColumn()
+    plotOptions.column.depth = 25
+    options.plotOptions = plotOptions
+
+    let xAxis = HIXAxis()
+    xAxis.categories = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    xAxis.labels = HILabels()
+    xAxis.labels.skew3d = true
+    xAxis.labels.style = HICSSObject()
+    xAxis.labels.style.fontSize = "16px"
+    options.xAxis = [xAxis]
+
+    let column = HIColumn()
+    column.name = "Sales"
+    column.data = [2, 3, NSNull(), 4, 0, 5, 1, 4, 6, 3]
+    options.series = [column]
+
+    chartView.options = options
+
+    self.view.addSubview(chartView)
+  }
+
+}
