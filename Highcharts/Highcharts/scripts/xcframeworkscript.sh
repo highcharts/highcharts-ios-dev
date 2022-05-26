a753cd8adb8aa48e62439eb2587bde6157b9f24f
Ihnat Moisieiev <ihnat.moisieiev@gmail.com>
2022-05-26 23:52:19 +0200

    Mac Catalyst separate build

--- Highcharts/Highcharts/scripts/xcframeworkscript.sh
+++ Highcharts/Highcharts/scripts/xcframeworkscript.sh
@@ -14,20 +14,10 @@ xcodebuild archive \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-# Mac Catalyst
-xcodebuild archive \
-    -scheme Highcharts \
-    -archivePath "./XCFramework/Archive-Mac-Catalyst.xcarchive" \
-    -destination 'platform=macOS,arch=x86_64,variant=Mac Catalyst' \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-
 xcodebuild -create-xcframework \
     -framework "./XCFramework/Archive-iOS.xcarchive/Products/Library/Frameworks/Highcharts.framework" \
     -framework "./XCFramework/Archive-iOS-Simulator.xcarchive/Products/Library/Frameworks/Highcharts.framework" \
-    -framework "./XCFramework/Archive-Mac-Catalyst.xcarchive/Products/Library/Frameworks/Highcharts.framework" \
     -output "./XCFramework/Highcharts.xcframework"
 
 rm -dr ./XCFramework/Archive-iOS.xcarchive
 rm -dr ./XCFramework/Archive-iOS-Simulator.xcarchive
-rm -dr ./XCFramework/Archive-Mac-Catalyst.xcarchive
